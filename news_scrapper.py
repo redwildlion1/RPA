d5a25670222324b3dab14a0681c55bba031f86b8
redwildlion1 <redwildlion1@yahoo.com>
2024-07-31 20:10:15 +0300

    variables are killing me

--- news_scrapper.py
+++ news_scrapper.py
@@ -91,10 +91,12 @@ class NewsScraper:
         self.excel.close_workbook()
 
 if __name__ == "__main__":
-    parameters = workitems.inputs()
-    search_phrase = parameters['search_phrase']
-    news_category = parameters['news_category']
-    num_months = int(parameters['num_months'])
+    parameters = workitems.inputs
+    for item in parameters:
+        print(item.payload)
+    search_phrase = parameters[0].payload.value
+    news_category = parameters[1].payload.value
+    num_months = parameters[2].payload.value
     # Replace with actual parameters
     scraper = NewsScraper(search_phrase=search_phrase, news_category=news_category, num_months=num_months)
     try:
